d88ffed71a8992e4adf45b26177b46b3b214e4a9
Asaf Ben Shabat <68661307+BlackChesire@users.noreply.github.com>
2022-03-03 17:25:09 +0200

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,12 +2,7 @@ import firebase from 'firebase/app'
 import 'firebase/firestore'
 import 'firebase/auth'
 const firebaseConfig = {
-  apiKey: "AIzaSyBHO3XXoQvrd-rO7_FAm0HgPyuI4PgqTkg",
-  authDomain: "finance-tracker-web.firebaseapp.com",
-  projectId: "finance-tracker-web",
-  storageBucket: "finance-tracker-web.appspot.com",
-  messagingSenderId: "768657053276",
-  appId: "1:768657053276:web:ee9d4d6dde866d8f5cf899"
+  //firebase API KEY
 };
 
 // init firebase
